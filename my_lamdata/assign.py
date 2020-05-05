cc46b1608f15511614387ee834263b8a5eb24d4e
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-05-05 17:48:58 -0400

    functions made

---
+++ my_lamdata/assign.py
@@ -0,0 +1,51 @@
+# helper function: checking for nan values
+
+import pandas 
+
+
+def null_val(x):
+    """
+    Checks for null values in any given Pandas
+    Dataframe Object.
+
+    Args:
+        x;A Pandas dataframe object
+    
+    Returns:
+            Null value counts for each column
+
+
+
+
+
+    """
+    
+    x = x.copy()
+    nulls = pandas.Series(x.isnull().sum())
+    print('Null Values in each Column')
+    print('---------------------------')
+    print(nulls)
+    print('---------------------------')
+
+def cat_variables(x):
+    """
+    Checks for categorical variables in 
+    any given DF
+
+    args: 
+        x; Pandas Dataframe Object
+    returns
+        list of all categorical data
+
+
+
+
+    """
+    x = x.copy()
+    result = x.astype('object').describe()  
+    return result 
+
+    if __name__ == "__main__":
+        
+        print('if you imported this function, you did,something wrong')
+    
