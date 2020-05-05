09c32fad14f1c8e06174e0378cdb5674f395df31
LuisFelipeUrena <60860402+LuisFelipeUrena@users.noreply.github.com>
2020-05-05 19:46:52 -0400

    new version

--- my_lamdata/setup.py
+++ my_lamdata/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-luife", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Luis Felipe Urena Hernandez",
     author_email="luis-felipe-urena-hernandez@lambdastudents.com",
     description="Functions made for 05/4/2020 assignment",
